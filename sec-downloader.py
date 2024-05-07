b3d321f49027f4c9243c15eccc7d857850aae9d1
sugun <sugun119@gmail.com>
2024-05-07 17:09:25 -0400

    Got simple directory reader to successfully

--- sec-downloader.py
+++ sec-downloader.py
@@ -1,15 +1,33 @@
+from dotenv import load_dotenv
+import openai
+import os
 from langchain_openai import OpenAI
 from sec_edgar_downloader import Downloader
-import os
-from llama_index import SimpleDirectoryReader, ServiceContext, VectorStoreIndex
-from llama_index import set_global_service_context
-from llama_index.response.pprint_utils import pprint_response
-from llama_index.tools import QueryEngineTool, ToolMetadata
-from llama_index.query_engine import SubQuestionQueryEngine
+from llama_index.core import SimpleDirectoryReader, ServiceContext, VectorStoreIndex
+from llama_index.core import set_global_service_context
+from llama_index.core.response.pprint_utils import pprint_response
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.query_engine import SubQuestionQueryEngine
+
+openai.api_key = 'sk-proj-hKQC4BE0U48yugBnJbc2T3BlbkFJTyNFZUpT1vjiw2u1r8AQ'
+
+
+a = SimpleDirectoryReader(input_dir="sec_filings/sec-edgar-filings/AAPL/10-k", recursive=True).load_data()
+b = SimpleDirectoryReader(input_dir="sec_filings/sec-edgar-filings/MSFT/10-k", recursive=True).load_data()
+print(f'Loaded apple 10-K with {len(a)} pages')
+print(f'Loaded microsoft 10-K with {len(b)} pages')
+
+llm = OpenAI(temperature=0, model_name="gpt-4",  max_tokens=-1)
+
+service_context = ServiceContext.from_defaults(llm=llm)
+set_global_service_context(service_context=service_context)
+
+
+
 
 
 # List of company tickers
-companies = ['AAPL', 'MSFT', 'GOOG']  # replace with your companies
+companies = ['AAPL', 'MSFT']  # replace with your companies
 
 # Directory to save filings
 dir_to_save = 'sec_filings'
@@ -25,5 +43,8 @@ for company in companies:
     # Loop over each year from 1995 through 2023
         # Download the 10-K filings
         dl.get("10-K", company, limit=29, after=f"{year}-01-01", before=f"{year}-12-31")
+
+print(f'Loaded lyft 10-K with {len(a)} pages')
+print(f'Loaded Uber 10-K with {len(b)} pages')
 
 
