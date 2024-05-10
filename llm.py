366d2d4bbd56d3654ba935a4df833bcc74053a03
sugun <sugun119@gmail.com>
2024-05-10 18:09:12 -0400

    update Simpledirectoryreader to be generalized to company ticker input

--- llm.py
+++ llm.py
@@ -11,28 +11,21 @@ from llama_index.core import set_global_service_context
 from llama_index.core.response.pprint_utils import pprint_response
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.core.query_engine import SubQuestionQueryEngine
+from sec_downloader import a,b
 
 api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = api_key
 
 client = oai(organization='org_key')
 
-llm = oai(temperature=0, model_name="gpt-3.5-turbo-instruct", max_tokens=1)
-service_context = ServiceContext.from_defaults(llm=llm)
-set_global_service_context(service_context=service_context)
-
-a = SimpleDirectoryReader(input_dir="sec_filings/sec-edgar-filings/AAPL/10-k", recursive=True).load_data()
-b = SimpleDirectoryReader(input_dir="sec_filings/sec-edgar-filings/MSFT/10-k", recursive=True).load_data()
-
-print(f'Loaded Apple 10-K with {len(a)} pages')
-print(f'Loaded Microsoft 10-K with {len(b)} pages')
-
-
-"""Settings.llm = oai(model="text-embedding-3-small", temperature=0)
+llm = oai(temperature=0, model_name="gpt-3.5-turbo-instruct", max_tokens=1, api_key=openai.api_key)
+Settings.llm = oai(model="text-embedding-3-small", temperature=0)
 Settings.embed_model = OpenAIEmbedding(
     model="text-embedding-3-small", embed_batch_size=50
 )
-Settings.node_parser = SentenceSplitter(chunk_size=256, chunk_overlap=10)"""
+Settings.node_parser = SentenceSplitter(chunk_size=256, chunk_overlap=10)
+
+
 
 
 a_index = VectorStoreIndex.from_documents(a, show_progress=True, insert_batch_size=512)
